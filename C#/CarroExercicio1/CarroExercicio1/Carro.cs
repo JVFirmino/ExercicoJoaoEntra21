a333357e33ae812f30481b92dacfa376df803206
JVFirmino <joaov3865@gmail.com>
2023-11-27 00:14:30 -0300

    Exercicio 1 utilizando a classe Carro com metodo Construtor, Propriedade e validação no set

---
+++ C#/CarroExercicio1/CarroExercicio1/Carro.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarroExercicio1
+{
+    internal class Carro
+    {
+     
+        private string placa;
+        public string Placa
+        {
+            get
+            {
+                return placa;
+            }
+            set
+            {
+                if (value.Length >= 4)
+                {
+                    placa = value;
+                }
+                else
+                {
+                    placa = "Valor Inválido";
+                }
+            }
+        }
+
+        private string marca;
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                if (value.Length >= 4)
+                {
+                    marca = value;
+                }
+                else
+                {
+                    marca = "Valor Inválido";
+                }
+            }
+        }
+
+        private string modelo;
+        public string Modelo
+        {
+            get
+            {
+                return modelo;
+            }
+            set
+            {
+                if (value.Length >= 4)
+                {
+                    modelo = value;
+                }
+                else
+                {
+                    modelo = "Valor Inválido";
+                }
+            }
+        }
+
+        private string cor;
+        public string Cor
+        {
+            get
+            {
+                return cor;
+            }
+            set
+            {
+                if (value.Length >= 4)
+                {
+                    cor = value;
+                }
+                else
+                {
+                    cor = "Valor Inválido";
+                }
+            }
+        }
+
+        public Carro(string placa, string marca, string modelo, string cor)
+        {
+            Placa = placa;
+            Marca = marca; ;
+            Modelo = modelo;
+            Cor = cor;
+        }
+
+        public string mostrarCarro()
+        {
+            return $"Placa: {placa}, Marca: {marca}, Modelo: {modelo} Cor: {cor}";
+        }
+    }
+}
---
+++ C#/CarroExercicio1/CarroExercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CarroExercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro("4343-123A", "Fiat", "Uno Bolinha", "Preto");
+            Console.WriteLine(carro.mostrarCarro());
+            Console.ReadKey();
+        }
+    }
+}
