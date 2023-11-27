effe6e31ea2271fb46567a121b91f8ee959f2358
JVFirmino <joaov3865@gmail.com>
2023-11-27 00:22:09 -0300

    Exercicio 2 utilizando a Classe Calculadora com os atributos X e Y com os metodos adicao, subtracao, divisao e multiplicacao

---
+++ C#/CalculadoraExercio02/CalculadoraExercio02/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraExercio02
+{
+    internal class Calculadora
+    {
+        public int x;
+        public int y;
+
+        public void adicao()
+        {
+            Console.WriteLine($"Adição: {x + y}");
+        }
+        public void subtracao()
+        {
+            Console.WriteLine($"Subtração: {x - y}");
+        }
+        public void multiplicacao()
+        {
+            Console.WriteLine($"Multiplicação: {x * y}");
+        }
+        public void divisao()
+        {
+            Console.WriteLine($"Divisão: {x / y}");
+        }
+    }
+}
---
+++ C#/CalculadoraExercio02/CalculadoraExercio02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculadoraExercio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            calculadora.x = 100;
+            calculadora.y = 20;
+            calculadora.adicao();
+            calculadora.adicao();
+            calculadora.multiplicacao();
+            calculadora.divisao();
+            Console.ReadKey();
+        }
+    }
+}
