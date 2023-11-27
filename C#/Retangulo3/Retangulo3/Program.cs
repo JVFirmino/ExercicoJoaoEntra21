96125dc881045053db1a9eb4ae8b6ae3d8e8107e
JVFirmino <joaov3865@gmail.com>
2023-11-27 01:28:52 -0300

    Exercicio 3 utilizando 2 atributos altura e comprimento com dois metodos MostrarPerimetro e MostrarArea

---
+++ C#/Retangulo3/Retangulo3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Retangulo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            retangulo.altura = 5;
+            retangulo.comprimento = 10;
+
+            Console.WriteLine(retangulo.MostrarPerimetro());
+            Console.WriteLine(retangulo.MostrarArea());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Retangulo3/Retangulo3/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo3
+{
+    internal class Retangulo
+    {
+        public double comprimento;
+        public double altura;
+
+        public string MostrarPerimetro()
+        {
+            return $"Perímetro: {comprimento * 2 + altura * 2}";
+        }
+        public string MostrarArea()
+        {
+            return $"Área: {comprimento * altura}";
+        }
+    }
+}
