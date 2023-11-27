911e578583936bd0d2d17e3bf883bac7ed063e71
JVFirmino <joaov3865@gmail.com>
2023-11-27 01:29:53 -0300

    Exercicio 3 utilizando metodo Construtor, Propriedades e validação no Set e com os metodos CalculoPerimetro e CalculoArea

---
+++ C#/Retangulo03/Retangulo03/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Retangulo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo(6, 8);
+
+            retangulo.CalculoArea();
+            retangulo.CalculoPerimetro();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Retangulo03/Retangulo03/Retangulo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo03
+{
+    internal class Retangulo
+    {
+        private int altura;
+        private int comprimento;
+
+        public int Altura
+        {
+            get
+            {
+                return altura;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    altura = value;
+                }
+            }
+        }
+        public int Comprimento
+        {
+            get
+            {
+                return comprimento;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    comprimento = value;
+                }
+            }
+        }
+
+        public Retangulo(int altura, int comprimento)
+        {
+            Altura = altura;
+            Comprimento = comprimento;
+        }
+
+        public void CalculoPerimetro()
+        {
+            if (altura != 0 && comprimento != 0)
+            {
+                Console.WriteLine($"Perimetro: {(altura + comprimento) * 2}");
+            }
+            else
+            {
+                Console.WriteLine("Reveja os valores");
+            }
+        }
+        public void CalculoArea()
+        {
+            if (altura != 0 && comprimento != 0)
+            {
+                Console.WriteLine($"Área: {comprimento * altura}");
+            }
+            else
+            {
+                Console.WriteLine("Reveja os valores");
+            }
+        }
+    }
+}
