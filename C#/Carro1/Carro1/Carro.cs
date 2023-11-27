5760e4ae2b9d670f685838bca74d2d37cc7dec1c
JVFirmino <joaov3865@gmail.com>
2023-11-27 00:56:33 -0300

    Exercicio 1 utilizando 4 atributos placa, marca, modelo e cor e com o metodo MostararCarro

---
+++ C#/Carro1/Carro1/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carro1
+{
+    internal class Carro
+    {
+        public string placa;
+        public string marca;
+        public string modelo;
+        public string cor;
+
+        public void mensagem()
+        {
+            Console.WriteLine($"Placa: {placa} Marca:{marca} Modelo:{modelo} Cor:{cor}");
+
+        }
+    }
+}
---
+++ C#/Carro1/Carro1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Carro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.placa = "4343-123A";
+            carro.marca = "Fiat";
+            carro.modelo = "Uno Bolinha";
+            carro.cor = "Preto";
+            carro.mensagem();
+            Console.ReadKey();
+        }
+    }
+}
