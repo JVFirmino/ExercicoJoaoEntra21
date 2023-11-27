a647d0c1abf16e15d8908b0f7e969281a75565b4
JVFirmino <joaov3865@gmail.com>
2023-11-27 03:03:25 -0300

    Criando um SortedList com a Key recebendo uma categoria do tipo string e o Value recebe um List que recebe um objeto do tipo Produto

---
+++ C#/ColecaoProdutos/ColecaoProdutos/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ColecaoProdutos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+
+            Produto produto1 = new Produto("Pão");
+            produtos.Add(produto1);
+
+            Produto produto2 = new Produto("Aveia");
+            produtos.Add(produto2);
+
+            SortedList<string, List<Produto>> categoriaProduto = new SortedList<string, List<Produto>>();
+            categoriaProduto.Add("Cereais", produtos);
+
+            foreach (KeyValuePair<string, List<Produto>> kvg in categoriaProduto)
+            {
+                foreach (Produto produto in kvg.Value)
+                {
+                    Console.WriteLine($"{kvg.Key} : {produto.Nome}");
+                }
+            }
+        }
+    }
+}
