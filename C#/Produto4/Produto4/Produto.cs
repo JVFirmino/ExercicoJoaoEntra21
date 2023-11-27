17bb06373947e818ed3512810d4e9cf3c4fd29ce
JVFirmino <joaov3865@gmail.com>
2023-11-27 01:46:09 -0300

    Exercicio 4 utilizando 4 atributos codigo, Descricao, Estoque e ValorUnitario com o metodo Mostrar

---
+++ C#/Produto4/Produto4/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto4
+{
+    internal class Produto
+    {
+        public int Codigo;
+        public string Descricao;
+        public int Estoque = 10;
+        public double ValorUnitario;
+        double valorDesconto;
+        double valorTaxa;
+        double valorTotal;
+        public void Mostrar()
+        {
+            valorDesconto = (ValorUnitario / 100) * 10;
+            valorTaxa = (ValorUnitario / 100) * 20;
+            valorTotal = ValorUnitario + valorTaxa - valorDesconto;
+            Console.WriteLine($"Código: {Codigo} Descrição: {Descricao} Estoque: {Estoque}");
+            Console.WriteLine($"Valor Unitário: {ValorUnitario} Valor desconto: {valorDesconto} Valor taxa: {valorTaxa} Valor total: {valorTotal} ");
+        }
+    }
+}
---
+++ C#/Produto4/Produto4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Produto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            produto.Codigo = 1;
+            produto.Descricao = "Chocolate";
+            produto.ValorUnitario = 20;
+            produto.Mostrar();
+            Console.ReadKey();
+        }
+    }
+}
