bd1544f8617db759e21f74e273fe6bd49b79b421
JVFirmino <joaov3865@gmail.com>
2023-11-27 02:38:10 -0300

    Criando uma List de Produtos que não permite produtos duplicados

---
+++ C#/ListNãoProdDuplicados/ListNãoProdDuplicados/Produtos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListNãoProdDuplicados
+{
+    internal class Produtos
+    {
+        public string Nome { get; set; }
+        public Produtos() { }
+        public Produtos(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void Adicionando(List<Produtos> Lista)
+        {
+            Console.WriteLine("Digite o nome do Produto: ");
+            string nome = Console.ReadLine();
+            foreach (var item in Lista)
+            {
+                while (nome == item.Nome)
+                {
+                    Console.WriteLine("Digite o nome do Produto: ");
+                    nome = Console.ReadLine();
+                }
+            }
+
+            Produtos produtos = new Produtos();
+            produtos.Nome = nome;
+
+            Lista.Add(produtos);
+        }
+        public void Listando(List<Produtos> Lista)
+        {
+            foreach (var item in Lista)
+            {
+                Console.WriteLine($"{item.Nome}");
+            }
+        }
+    }
+}
---
+++ C#/ListNãoProdDuplicados/ListNãoProdDuplicados/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListNãoProdDuplicados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos produtos = new Produtos();
+            List<Produtos> lista = new List<Produtos>();
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Deseja continuar adicionando produto - Sim[S] / Não[N] ou Mostrar[M]");
+                string escolha = Console.ReadLine().ToUpper();
+                switch (escolha)
+                {
+                    case "S":
+                        produtos.Adicionando(lista);
+                        break;
+                    case "N":
+                        continuar = false;
+                        break;
+                    case "M":
+                        produtos.Listando(lista);
+                        break;
+                    default:
+                        Console.WriteLine("Digite N ou S");
+                        break;
+                }
+            }
+            Console.WriteLine("Obrigado!!!");
+        }
+    }
+}
