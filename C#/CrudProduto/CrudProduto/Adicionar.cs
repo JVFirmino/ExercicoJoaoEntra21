22f52bac23a27914070209f776c48a5ced6aebdc
JVFirmino <joaov3865@gmail.com>
2023-11-27 02:01:09 -0300

    Crud do exercicio de Produtos com Lista tipada

---
+++ C#/CrudProduto/CrudProduto/Adicionar.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CrudProduto
+{
+    internal class Produtos
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int Estoque { get; set;}
+        public double ValorUnitario { get; set;}
+
+        public Produtos()
+        {
+
+        }
+
+        public Produtos(int codigo, string descricao, int estoque, double valorUnitario) 
+        { 
+            this.Codigo = codigo;
+            this.Descricao = descricao;
+            this.Estoque = estoque;
+            this.ValorUnitario = valorUnitario;
+      
+        }
+
+        public void Adicionar(List<Produtos> produtos)
+        {
+           
+
+            Console.WriteLine("Código Produto");
+            int codigo = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var item in produtos)
+            {
+               while (codigo == item.Codigo)
+               {
+                    Console.WriteLine("Código Produto");
+                    codigo = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Descrição Produto");
+            string descricao = Console.ReadLine();
+
+            Console.WriteLine("Estoque do Produto");
+            int estoque = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Valor Unitário do Produto");
+            double valorUnitario = Convert.ToDouble(Console.ReadLine());
+
+            Produtos produtoss = new Produtos(codigo, descricao, estoque, valorUnitario);
+
+            produtos.Add(produtoss);  
+
+        } 
+        public void listando(List<Produtos> produtos)
+        {
+            foreach (var item in produtos)
+            {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine($"Código: {item.Codigo}");
+                Console.WriteLine($"Descrição: {item.Descricao}");
+                Console.WriteLine($"Estoque: {item.Estoque}");
+                Console.WriteLine($"Valor Unitário: {item.ValorUnitario}");
+                Console.WriteLine("-----------------------------------------------");
+            }
+        }
+
+        public void remover(List<Produtos> produtos)
+        {
+            bool removido = true;
+            Produtos itens = null;
+            Console.WriteLine("Código Produto");
+            int rcodigo = Convert.ToInt32(Console.ReadLine());
+
+            foreach (Produtos item in produtos)
+            {
+                if (rcodigo == item.Codigo)
+                {
+                    itens = item;
+                    removido = false;
+                }
+            }
+            if(removido == false)
+            {
+                produtos.Remove(itens);
+            }
+        }
+
+        public void alterar(List<Produtos> produtos)
+        {
+            Console.WriteLine("Código Produto");
+            int acodigo = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in produtos)
+            {
+                if(acodigo == item.Codigo)
+                {
+                    Console.WriteLine("Código Produto");
+                    int codigoa = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Descrição Produto");
+                    string adescricao = Console.ReadLine();
+
+                    Console.WriteLine("Estoque do Produto");
+                    int aestoque = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Valor Unitário do Produto");
+                    double avalorUnitario = Convert.ToDouble(Console.ReadLine());
+
+                    item.Codigo = codigoa;
+                    item.Descricao = adescricao;
+                    item.Estoque = aestoque;
+                    item.ValorUnitario = avalorUnitario;
+                    break;
+                }        
+            }
+        }
+    }
+}
---
+++ C#/CrudProduto/CrudProduto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace CrudProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Produtos produtoss = new Produtos();
+            List<Produtos> produtos = new List<Produtos>();   
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Escolha Ver Produtos[1]  Adicionar Produtos[2]  Remover Produtos[3]  Alterar Produto[4] [5]Sair");
+                int escolher = Convert.ToInt32(Console.ReadLine());   
+
+                switch (escolher)
+                {
+                    case 1:
+                        produtoss.listando(produtos);
+                        break;
+                    case 2:
+                        produtoss.Adicionar(produtos);           
+                        break;
+                    case 3:
+                        produtoss.remover(produtos);
+                        break;
+                    case 4:
+                        produtoss.alterar(produtos);
+                        break;
+                    case 5:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+
+            }
+            Console.WriteLine("Obrigado!!!");
+
+        }
+
+    }
+}
