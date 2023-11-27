897d61e3b0c110eb07b810fbf0f6a478f7905a22
JVFirmino <joaov3865@gmail.com>
2023-11-27 02:49:52 -0300

    Criando uma SortedList com a Key recebendo o nome do tipo String e o Value recebe um objeto do tipo Pessoa

---
+++ C#/ColecaoPessoa/ColecaoPessoa/Pessoas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColecaoPessoa
+{
+    internal class Pessoas
+    {
+        public string Pessoa { get; set; }
+
+        public Pessoas() { }
+
+        public Pessoas(string pessoa)
+        {
+            this.Pessoa = pessoa;
+        }
+        public void Adicionando(SortedList<string, Pessoas> SortPessoas)
+        {
+            Console.WriteLine("Id Pessoa");
+            string id = Console.ReadLine();
+            Console.WriteLine("Nome Pessoa");
+            string nome = Console.ReadLine();
+
+            Pessoas pessoa = new Pessoas();
+            pessoa.Pessoa = nome;
+            SortPessoas.Add(id, pessoa);
+
+        }
+        public void Listando(SortedList<string, Pessoas> SortPessoas)
+        {
+            foreach (KeyValuePair<string, Pessoas> kvp in SortPessoas)
+            {
+                Console.WriteLine($"Key: {kvp.Key}  Values: {kvp.Value.Pessoa}");
+            }
+        }
+    }
+}
---
+++ C#/ColecaoPessoa/ColecaoPessoa/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ColecaoPessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas pessoas = new Pessoas();
+            SortedList<string, Pessoas> SortPessoas = new SortedList<string, Pessoas>();
+
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Adicionar Pessoas [1]Adicionar [2]Mostrar [3]Sair");
+                int escolher = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolher)
+                {
+                    case 1:
+                        pessoas.Adicionando(SortPessoas);
+                        break;
+                    case 2:
+                        pessoas.Listando(SortPessoas);
+                        break;
+                    case 3:
+                        continuar = false;
+                        break;
+                }
+            }
+            Console.WriteLine("Obrigado!!!");
+        }
+    }
+}
